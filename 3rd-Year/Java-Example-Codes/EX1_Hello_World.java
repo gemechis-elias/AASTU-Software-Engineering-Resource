737b4f6fae98c6b3db6d283b20548fc464a3240a
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2022-12-04 09:01:55 +0300

    Java Examples

---
+++ 3rd-Year/Java-Example-Codes/EX1_Hello_World.java
@@ -0,0 +1,21 @@
+public class EX1_Hello_World 
+{
+	
+//Main Method : Every Program Execution Starts from Main Method
+	public static void main(String[] args) 
+	{
+		//For Printing Statement we make use of "System.out.print();"
+		System.out.println("Hello World");
+	}
+
+}
+
+/* OUTPUT :
+
+
+    Hello World
+
+*/
+
+
+
---
+++ 3rd-Year/Java-Example-Codes/EX3_User_Input.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class EX3_User_Input
+{
+	public static void main(String[] args) 
+	{
+		int a;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the Value : ");
+		
+		a = sc.nextInt();
+		
+		System.out.println(a);
+		
+	
+	}
+
+}
+
+// Output:
+/*
+  
+ 
+Enter the Value : 100
+100
+
+ 
+
+ 
+ */
