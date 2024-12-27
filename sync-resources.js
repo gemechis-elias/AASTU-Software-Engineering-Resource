c5b604de92708f4c2111e9632ed3616ca9fd8f5d
gemechis-elias <gemcs.elias@gmail.com>
2024-12-27 11:06:56 +0300

    [fix] Sync direcotry

--- sync-resources.js
+++ sync-resources.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-const sourceDir = path.resolve(__dirname, '../resources');
-const targetDir = path.resolve(__dirname, './website/docs');
+const sourceDir = './resources';
+const targetDir = './website/docs';
+
+console.log('Source Directory:', sourceDir);
+console.log('Target Directory:', targetDir);
 
 function syncResources(source, target) {
+  if (!fs.existsSync(source)) {
+    console.error(`Source directory "${source}" does not exist.`);
+    process.exit(1);
+  }
   fs.readdirSync(source, { withFileTypes: true }).forEach((entry) => {
     const sourcePath = path.join(source, entry.name);
     const targetPath = path.join(target, entry.name);
@@ -25,4 +32,4 @@ if (!fs.existsSync(targetDir)) {
 }
 
 syncResources(sourceDir, targetDir);
-console.log('✅ Resources synced to website/docs successfully!');
+console.log('✅ Resources synced successfully!');
