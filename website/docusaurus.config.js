f25a6cac6a5dbe2fbe1444d5e87d58dc11cf3a7f
gemechis-elias <gemcs.elias@gmail.com>
2024-12-27 12:17:31 +0300

    Updated Content of Static Website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -10,7 +10,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'AASTU Software Engineering Resource',
+  title: 'Software Eng. Resource',
   tagline: 'Addis Ababa Science and Technology University',
   favicon: 'img/favicon.ico',
 
