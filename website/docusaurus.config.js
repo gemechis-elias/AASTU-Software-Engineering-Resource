60459c1645642a420bdd1de8454fcc15966d0564
gemechis-elias <gemcs.elias@gmail.com>
2024-12-27 11:55:47 +0300

    Updated Content of Static Website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'aastu-software-resource.vercel.app',
+  url: 'https://aastu-software-resource.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
